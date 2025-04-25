379bc39c23b7f35c04e2c8b2b943d675d802d1fa
pietr <pietrokucharski@hotmail.com>
2025-04-25 16:31:41 -0300

    Criando e testando classe de entidade Seller

--- src/model/application/Program.java
+++ src/model/application/Program.java
@@ -1,11 +1,16 @@
 package model.application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
+        Seller seller = new Seller(21, "Pietro", "pietro@email.com", LocalDate.now(), 3000.0, department);
         System.out.println(department);
+        System.out.println(seller);
 
     }
 }
