4aa9335bf196e6868b04dc8fc8690909b5accbdf
pietr <pietrokucharski@hotmail.com>
2025-04-25 16:59:26 -0300

    Implementando e testando findById

--- src/model/application/Program.java
+++ src/model/application/Program.java
@@ -2,17 +2,13 @@ package model.application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
-        Department department = new Department(1, "Books");
-        Seller seller = new Seller(21, "Pietro", "pietro@email.com", LocalDate.now(), 3000.0, department);
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        System.out.println(department);
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
 
     }
